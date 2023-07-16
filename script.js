4863ab450af1f4b08623065fb8300370bd81a8aa
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2023-07-16 15:09:25 -0500

    update

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').toggleAttribute('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// Typing Effect
+
+let typed = Typed('.auto-input',{
+    strings: ['Front-End Developper!', 'Freelancer!', 'UI Designer!', 'YouTuber!'],
+    typedpeed: 100,
+    backspeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
