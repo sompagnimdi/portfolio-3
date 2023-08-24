ab7ff85ca3a914a6fa02dd1b99ebe43acafa4d49
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2023-08-24 00:00:48 -0500

    last commit

--- script.js
+++ script.js
@@ -27,13 +27,16 @@ let sections = document.querySelectorAll('section')
 console.log(sections)
 
 window.addEventListener('scroll', function (){
-    const scrollPos = this.window.scrollY
+    const scrollPos = window.scrollY + 20
     sections.forEach(section => {
         if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
             navLinks.forEach(link => {
                 link.classList.remove('active');
-            })
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
         }
-    }) 
-})
+    }); 
+});
 
